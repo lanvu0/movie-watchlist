729b3f6ea70bc1fe8c9051a0816314885ab2af4d
Lan <103476770+lanvu0@users.noreply.github.com>
2025-09-14 21:43:16 +1000

    Finish initial home screen

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-
+const mainContainer = document.querySelector('main')
 
 
 fetch('http://www.omdbapi.com/?i=tt3896198&apikey=dfef5489')
     .then(res => res.json())
     .then(data => console.log(data))
+
+
+
+function renderMain() {
+    mainContainer.style.background = `url('start-exploring.png')`
+    mainContainer.style.backgroundRepeat = "no-repeat"
+    mainContainer.style.backgroundPosition = "center"
+    mainContainer.style.backgroundSize = "15rem"
+}
+
+renderMain()
